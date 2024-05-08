0f0c67512609eb8d7ca4f44b5250215715be77ea
Oscar <oscarlagatta@gmail.com>
2024-05-08 21:27:25 +0100

    Refactor state management in Post creation components
    State control and functions related to the creation of posts were moved from the PostList component to the NewPost component. The change simplifies the organizational structure and reduces the overhead in the PostList component making each component more autonomous and focused. Additionally, some unnecessary white spaces and return characters were removed.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,37 +1,21 @@
 import Post from "./Post";
 import styles from './PostList.module.css';
 import NewPost from "./NewPost.jsx";
-import {useState} from "react";
 import Modal from "./Modal.jsx";
 
 const PostList = ({isPosting, onStopPosting}) => {
-
-
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
     return (
         <>
             {
-                isPosting && <Modal onClose={onStopPosting}>
-                    <NewPost
-                        onBodyChange={bodyChangeHandler}
-                        onAuthorChange={authorChangeHandler}
-                        onCancel={onStopPosting}
-                    />
-                </Modal>
+                isPosting && (
+                    <Modal onClose={onStopPosting}>
+                        <NewPost
+                            onCancel={onStopPosting}
+                        />
+                    </Modal>
+                )
             }
             <ul className={styles.posts}>
-                <Post body={enteredBody} author={enteredAuthor}/>
                 <Post author='MARCELO' body='ReactJS IS AWESOME'/>
             </ul>
         </>
