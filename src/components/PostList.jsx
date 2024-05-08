2708dda27b19de79e2a0d9ce7d38601a75976af9
Oscar <oscarlagatta@gmail.com>
2024-05-08 21:33:22 +0100

    Implement add post functionality in PostList component
    The commit introduces the add post functionality in the PostList component. It includes methods to handle new post data and update the existing posts state. The NewPost component was also adjusted to call the add post handler after creating a new post.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,8 +2,15 @@ import Post from "./Post";
 import styles from './PostList.module.css';
 import NewPost from "./NewPost.jsx";
 import Modal from "./Modal.jsx";
+import {useState} from "react";
 
 const PostList = ({isPosting, onStopPosting}) => {
+    const [posts, setPosts] = useState([]);
+
+    function addPostHandler(postData) {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
+
     return (
         <>
             {
@@ -11,11 +18,13 @@ const PostList = ({isPosting, onStopPosting}) => {
                     <Modal onClose={onStopPosting}>
                         <NewPost
                             onCancel={onStopPosting}
+                            onAddPost={addPostHandler}
                         />
                     </Modal>
                 )
             }
             <ul className={styles.posts}>
+
                 <Post author='MARCELO' body='ReactJS IS AWESOME'/>
             </ul>
         </>
